df9c8fde7c2b75592612e9be6bcd6f379e9d626a
MohammadReza Salari <frishter.ms@gmail.com>
2018-05-12 22:46:56 +0430

    make content page rtl.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,14 +4,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
-
 import Master from './components/Master';
 
 
 render(
   <Router history={browserHistory}>
       <Route path="/" component={Master} >
-
+        
       </Route>
     </Router>,
         document.getElementById('app'));
--- resources/assets/js/components/Master.js
+++ resources/assets/js/components/Master.js
@@ -14,45 +14,31 @@ class Master extends Component {
   render(){
     return (
       <LocaleProvider locale={faIR}>
-      <Layout>
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
-    >
-      <div className="logo" ><img src="" /></div>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1">
-          <Icon type="user" />
-          <span className="nav-text">nav 1</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-          <span className="nav-text">nav 2</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="user" />
-          <span className="nav-text">nav 4</span>
-        </Menu.Item>
+  <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">منو ۳</Menu.Item>
+        <Menu.Item key="2">منو ۲</Menu.Item>
+        <Menu.Item key="3">منو ۱</Menu.Item>
       </Menu>
-    </Sider>
-    <Layout>
-      <Header style={{ background: '#000', padding: 0 }} >
-      {/* <Button type="dashed">Dashed</Button> */}
-      </Header>
-      <Content style={{ margin: '24px 16px 0' }}>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-          content
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©2016 Created by Ant UED
-      </Footer>
-    </Layout>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2016 Created by Ant UED
+    </Footer>
   </Layout>
   </LocaleProvider>
     )
